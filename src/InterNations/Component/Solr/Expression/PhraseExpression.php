d49f8d193bf187a8e15629ee75653fbb678250b0
Lars Strojny <lars@strojny.net>
2013-03-09 18:02:27 +0100

    Cleanup

--- src/InterNations/Component/Solr/Expression/PhraseExpression.php
+++ src/InterNations/Component/Solr/Expression/PhraseExpression.php
@@ -10,14 +10,6 @@ use InterNations\Component\Solr\Util;
  */
 class PhraseExpression extends Expression
 {
-    /**
-     * @param string $expr
-     */
-    public function __construct($expr)
-    {
-        parent::__construct(mb_strtolower($expr, 'UTF-8'));
-    }
-
     public function __toString()
     {
         return Util::quote($this->expr);
